56a8df558be050e1829df6ef98cf56bdfb698f49
Jansiranii <43954194+Jansiranii@users.noreply.github.com>
2021-06-23 20:19:16 +0530

    new commit

---
+++ NodeJS/server.js
@@ -0,0 +1,34 @@
+//1.
+var sql = require('mssql');
+//2.
+var config = {
+    server: 'localhost',
+    database: 'Company',
+    user: 'sa',
+    password: 'sa',
+    port: 1433
+};
+//3.
+function loadEmployees() {
+    //4.
+    var dbConn = new sql.Connection(config);
+    //5.
+    dbConn.connect().then(function () {
+        //6.
+        var request = new sql.Request(dbConn);
+        //7.
+        request.query("select * from EmployeeInfo").then(function (recordSet) {
+            console.log(recordSet);
+            dbConn.close();
+        }).catch(function (err) {
+            //8.
+            console.log(err);
+            dbConn.close();
+        });
+    }).catch(function (err) {
+        //9.
+        console.log(err);
+    });
+}
+//10.
+loadEmployees();
